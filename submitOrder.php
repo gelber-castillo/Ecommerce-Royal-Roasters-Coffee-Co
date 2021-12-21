009c21b506888ffa1072a0715fd3230dc7b28027
Gelber Castillo <0so@users.noreply.github.com>
2021-12-21 16:26:31 -0500

    Update submitOrder.php

--- submitOrder.php
+++ submitOrder.php
@@ -1,23 +1,13 @@
-<html>
-<head>
-  <?php
+<?php
   session_start();
   require_once('functions.php');
   include "header.php";
   check_valid_user();
-  ?>
-</head>
-<body>
-  <?php
+  
   $ordertime  = date('Y-m-d H:i:s');
   $error      = 0;
 
-  @ $db = new mysqli('localhost', 'root', '', 'RoyalRoasters');
-  if (mysqli_connect_errno()) {
-     echo "Error: Could not connect to database.  Please try again later.";
-     exit;
-  }
-
+  @ $db = DBconnect();
   $getcartitems = "select * from cart where userID=".$_SESSION['userID'];
   $result = $db->query($getcartitems);
   $cartarray = $result->fetch_all(MYSQLI_ASSOC);
@@ -27,31 +17,33 @@
     $userID   = $item["userID"];
     $coffeeID = $item["coffeeID"];
     $quantity = $item["quantity"];
-    //query to add order to the order table
-    $orderquery = "insert into orders (userID, ordertime, coffeeID, quantity) values
-    (".$userID.", '".$ordertime."', ".$coffeeID.", ".$quantity.")";
-    $result = $db->query($orderquery);
-    if(!$result){
-      $err+=1;
-    }
     //query to update inventory on coffee table
     $invquery = "select * from coffee where coffeeID=".$coffeeID;
     $result = $db->query($invquery);
     if(!$result){
       $err+=1;
     }
-    $invarray = mysqli_fetch_assoc($result);
-    $invquantity = $invarray['inventory'];
-    $updateinv = "update coffee set inventory=".$invquantity-$quantity." where coffeeID=".$coffeeID;
-    $result = $db->query($updateinv);
+    $invarray     = mysqli_fetch_assoc($result);
+    $invquantity  = $invarray['inventory'];
+    $coffeePrice  = $invarray['price'];
+    $totalinv     = $invquantity-$quantity;
+    $updateinv    = "update coffee set inventory=".$totalinv." where coffeeID=".$coffeeID;
+    $result       = $db->query($updateinv);
     if(!$result){
-      $err+=1;
+      $err=1;
+    }
+    //query to add order to the order table
+    $orderquery   = "insert into orders (userID, ordertime, coffeeID, quantity, total) values
+    (".$userID.", '".$ordertime."', ".$coffeeID.", ".$quantity.", ".$quantity*$coffeePrice.")";
+    $result       = $db->query($orderquery);
+    if(!$result){
+      $err=2;
     }
     //query to remove the item from the user's cart
     $deletecart = "delete from cart where userID in ('".$_SESSION['userID']."') and coffeeID=".$coffeeID;
-    $result = $db->query($deletecart);
+    $result     = $db->query($deletecart);
     if(!$result){
-      $err+=1;
+      $err=3;
     }
   }
   ?>
